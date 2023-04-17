9073abe65a6fec79c3b1efffcbce13b852f2c9be
o.surovtsev <o.surovtsev@spd-ukraine.com>
2023-04-17 17:02:00 +0300

    update adding chapter3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,17 @@
-import java.text.SimpleDateFormat
-import java.util.*
-
-/*
-defaultTasks("hello")
-
-tasks.register("hello") {
-    doLast {
-        println("Hello world")
-    }
-}
-*/
-val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mmss'Z'")
-
-simpleDateFormat.timeZone = TimeZone.getTimeZone("UTC")
-
-println("Hello world " + simpleDateFormat.format(Date()))
-
-fun sayHello() {
-    println("Hello from method")
+plugins {
+    kotlin("jvm") version "1.8.20"
 }
 
-sayHello()
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+repositories {
+    mavenCentral()
+}
+kotlin {
+    jvmToolchain(11)
+}
+
+task("hello").doLast {
+    println("Hello world")
+}
